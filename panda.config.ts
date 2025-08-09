60e244fef1dc9b660f4b969b012d1ce31b415e85
victorhugodevbr <victorhugovicentedev@hotmail.com>
2025-08-09 00:14:28 -0300

    feat: color as components

--- panda.config.ts
+++ panda.config.ts
@@ -8,4 +8,39 @@ export default defineConfig({
   exclude: [],
   jsxFramework: 'react',
   outdir: "styled-system",
+  theme: {
+    extend: {
+      tokens: {
+        colors: {
+          white: {
+            value: "#FFFFFF",
+          },
+          black: {
+            value: "#000000",
+          },
+          primary: {
+            value: "#B7047E",
+          },
+          secondary: {
+            value: "#2000E6",
+          },
+          primaryRubBank: {
+            value: "#6B7AE5",
+          },
+          secondaryRubBank: {
+            value: "#1D1C3E",
+          },
+          primaryMaranata: {
+            value: "#FFAA00",
+          },
+          secondaryMaranata: {
+            value: "#191919",
+          },
+          primaryBrasilCard: {
+            value: "#004B89",
+          },
+        },
+      },
+    },
+  },
 });
