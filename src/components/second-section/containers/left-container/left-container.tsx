664f55b1bb797a5d4e0cc0b146a6018311212739
victorhugodevbr <victorhugovicentedev@hotmail.com>
2025-08-12 21:55:34 -0300

    feat: add LeftContainer and RightContainer components for second section

---
+++ src/components/second-section/containers/left-container/left-container.tsx
@@ -0,0 +1,11 @@
+// src/components/Button.tsx
+import { styled } from 'styled-system/jsx';
+
+export const LeftContainer = styled('div', {
+  base: {
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    marginTop: ['8vh', '10vh', '11.85vh'],
+  },
+});
---
+++ src/components/second-section/containers/right-container/right-container.tsx
@@ -0,0 +1,15 @@
+// src/components/Button.tsx
+import { styled } from 'styled-system/jsx';
+
+export const RightContainer = styled('div', {
+  base: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'start',
+    justifyContent: 'start',
+    width: '100%',
+    // paddingTop: ['5.6vh', '7vh', '8.29vh'],
+    padding: '7vh 2.6vw 5.93vh 2.6vw',
+    gap: '2.78vh',
+  },
+});
